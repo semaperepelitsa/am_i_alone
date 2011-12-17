7e917a8b01133c9720ddbe07e8a007ec1d5a6500
Semyon Perepelitsa <sema@sema.in>
2011-12-17 20:07:30 +0800

    Add grass moving along with player

--- game.rb
+++ game.rb
@@ -9,14 +9,39 @@ class Game < Chingu::Window
 end
 
 class Play < Chingu::GameState
+  traits :viewport
+
   def setup
-    Player.create(x: 50, y: 300)
+    viewport.game_area = [ 0, 0,   3000, $window.height]
+    @parallax = Chingu::Parallax.new(:x => 0, :y => 0, :rotation_center => :top_left)
+    @parallax.add_layer(
+      :image => "grass.png",
+      :repeat_x => true,
+      :repeat_y => false,
+      # :damping => 5
+    )
+
+    @player = Player.create(x: 50, y: 300, zorder: 300)
     @bg = Color::GREEN
   end
 
   def draw
     super
     fill @bg
+    @parallax.draw
+  end
+
+  def update
+    super
+    viewport.center_around(@player)
+    @parallax.camera_x, @parallax.camera_y = self.viewport.x.to_i, self.viewport.y.to_i
+    @parallax.update
+  end
+end
+
+class Wall < Chingu::GameObject
+  def intitialize(options = {})
+    super(options.merge(image: Image['wall.png']))
   end
 end
 
@@ -25,7 +50,7 @@ class Player < Chingu::GameObject
   SPEED = 3
 
   def initialize(options = {})
-    super(options.merge(image: Image['player.png']))
+    super(options.merge(image: 'player.png'))
     self.input = [ :holding_left,
                    :holding_right,
                    :holding_up,
