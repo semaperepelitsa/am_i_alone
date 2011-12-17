5a246c3827184e902e5e6c12f703902a024254b8
Semyon Perepelitsa <sema@sema.in>
2011-12-17 20:25:55 +0800

    I should test it but don't know how yet

---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift(File.expand_path('../lib', __FILE__))
+$LOAD_PATH.unshift(File.expand_path('../lib/game_states', __FILE__))
+$LOAD_PATH.unshift(File.expand_path('../lib/game_objects', __FILE__))
+require "minitest/autorun"
