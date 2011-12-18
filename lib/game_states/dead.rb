ec73714eed3993fc39169329b07e31c7962cf8e6
Semyon Perepelitsa <sema@sema.in>
2011-12-19 03:28:02 +0800

    Add copyright

--- lib/game_states/dead.rb
+++ lib/game_states/dead.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require "chingu"
 require "play"
 
@@ -17,12 +18,14 @@ class Dead < Chingu::GameState
   def setup
     @font = Gosu::Font[35]
     @small = Gosu::Font[24]
+    @copyright = Gosu::Font[16]
   end
 
   def draw
     super
     @font.draw_rel("You've killed #{score} zombies", $window.width/2, $window.height/2, 9990, 0.5, 1.0)
     @font.draw_rel(score.description, $window.width/2, $window.height/2, 9990, 0.5, 0.0)
-    @small.draw_rel("Press Enter to try again", $window.width/2, $window.height-10, 9990, 0.5, 1.0)
+    @small.draw_rel("Press Enter to try again", $window.width/2, $window.height-60, 9990, 0.5, 1.0)
+    @copyright.draw_rel("© 2011 Semyon Perepelitsa http://sema.in", $window.width/2, $window.height-10, 9990, 0.5, 1.0)
   end
 end
