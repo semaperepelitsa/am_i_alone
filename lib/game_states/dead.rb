1115618184b6e18c1f35d1e09a66366735ffc377
Semyon Perepelitsa <sema@sema.in>
2011-12-19 01:28:33 +0800

    Add instruction on how to start again

--- lib/game_states/dead.rb
+++ lib/game_states/dead.rb
@@ -16,11 +16,13 @@ class Dead < Chingu::GameState
 
   def setup
     @font = Gosu::Font[35]
+    @small = Gosu::Font[24]
   end
 
   def draw
     super
     @font.draw_rel("You've killed #{score} zombies", $window.width/2, $window.height/2, 9990, 0.5, 1.0)
     @font.draw_rel(score.description, $window.width/2, $window.height/2, 9990, 0.5, 0.0)
+    @small.draw_rel("Press Enter to try again", $window.width/2, $window.height-10, 9990, 0.5, 1.0)
   end
 end
